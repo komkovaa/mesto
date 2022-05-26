c376a3ffb7be779ce6142bced2803e08db8ad706
komkova <komkova.ar@gmail.com>
2022-05-26 17:25:53 +0300

    Сверстать диалоговое окно

---
+++ script/script.js
@@ -0,0 +1,6 @@
+let editButton = document.querySelector('.profile__edit-button');
+console.log(editButton);
+
+editButton.addEventListener('click', function showPrompt("Редактировать профиль") {}
+    
+    );
