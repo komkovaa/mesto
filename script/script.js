278e58974fc1103244d4e316c1dcf2bbae447a75
komkova <komkova.ar@gmail.com>
2022-05-30 00:38:09 +0300

    Исправить после ревью

--- script/script.js
+++ script/script.js
@@ -1,14 +1,16 @@
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = popup.querySelector('.popup__close');
-const popupName = document.querySelector('.popup__name');
-const popupDesc = document.querySelector('.popup__desc');
-
+const popupName = document.querySelector('.popup__desc_elem_name');
+const popupJob = document.querySelector('.popup__desc_elem_job');
 const popupForm = popup.querySelector('.popup__container');
-
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
 
 function showPopup() {
     popup.classList.add('popup_opened');
+    popupName.value = profileName.textContent; // Получите значение полей jobInput и nameInput из свойства value
+    popupJob.value = profileJob.textContent;
 }
 
 function closePopup() {
@@ -20,19 +22,11 @@ function closePopup() {
 function formSubmitHandler(evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    popupName.getAttribute('value');
-    popupDesc.getAttribute('value');
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileName = document.querySelector('.profile__name');
-    let profileDescription = document.querySelector('.profile__description');
+    // О том, как это делать, расскажем позже. 
 
     // Вставьте новые значения с помощью textContent
-    profileName.textContent = popupForm.profileName.value;
-    profileDescription.textContent = popupForm.profileDesc.value;
+    profileName.textContent = popupName.value;
+    profileJob.textContent = popupJob.value;
     closePopup();
 }
 
